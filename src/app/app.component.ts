928f99eda82833819d4c13e2a6b064b2ad0cfa16
whyboris <yboris@yahoo.com>
2020-12-02 19:37:22 -0500

    socket connection works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,24 @@ export class AppComponent implements OnInit {
 
   items: ImageElement[]; // ImageElement[]
   searchString: string = '';
+  websocket: WebSocket;
+  socketConnected: boolean = false;
 
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit() {
+
+    // 'ws://localhost:8080' or for testing 'wss://echo.websocket.org');
+    const socketAddress: string = 'ws://' + window.location.hostname + ':8080';
+
+    this.websocket = new WebSocket(socketAddress);
+    this.websocket.onopen =    (event) => { this.onOpen(event)    };
+    this.websocket.onclose =   (event) => { this.onClose(event)   };
+    this.websocket.onmessage = (event) => { this.onMessage(event) };
+    this.websocket.onerror =   (event) => { this.onError(event)   };
+
     console.log('fetching data!');
 
     this.getImageList().subscribe((data: ImageElement[]) => {
@@ -41,6 +53,55 @@ export class AppComponent implements OnInit {
       .subscribe((response) => {
         console.log(response);
       });
+  }
+
+  /**
+   * Request from server the current gallery view
+   */
+  getLatestData() {
+    this.websocket.send('refresh-request'); // request
+  }
+
+  /**
+   * When socket connection succeeds
+   */
+  onOpen(a) {
+    console.log('socket opened:');
+    console.log(a);
+    this.socketConnected = true;
+    this.websocket.send('hi');
+  }
+
+  /**
+   * When a message arrives via socket connection
+   */
+  onMessage(a) {
+    console.log('message received:');
+    try {
+      const data: ImageElement[] = JSON.parse(a.data);
+      this.items = data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  /**
+   * When socket connection closes
+   */
+  onClose(a) {
+    console.log('CLOSING SOCKET');
+    console.log(a);
+    this.socketConnected = false;
+    // make a message appear with a button "reconnect"
+  }
+
+  /**
+   * When socket connection errors out
+   */
+  onError(a) {
+    console.log('ERROR IN CONNECTION');
+    console.log(a);
+    this.socketConnected = false;
   }
 
 }
